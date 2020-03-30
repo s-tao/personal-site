fd9cb3879c1e85667fc0f53750fa6d14e3e866aa
Sarah Tao <satao.94@gmail.com>
2020-03-29 20:08:33 -0700

    revise layout w/ rwd for homepage-profile states

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,51 +1,50 @@
 import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithubSquare, faBuffer } from '@fortawesome/free-brands-svg-icons'
 import profilePic from '../prof-pic.jpg' 
-import Fade from 'react-reveal/Fade';
+import '../Homepage.css';
+
+// import Fade from 'react-reveal/Fade';
 
 // import { BrowserRouter as Router, Link, Redirect } from 'react-router-dom';
 
-// icons created as props 
-// const iconLinkedin = <FontAwesomeIcon icon={faLinkedin} />
-// const iconGithub = <FontAwesomeIcon icon={faGithubSquare} />
-// const iconProject = <FontAwesomeIcon icon={faBuffer} />
-
-// const [project, displayProject] = useState('');
 
 const Homepage = ({clickHandler}) => {
     return (
         // <Fade>
           <Container fluid className="homepage-info">
-            <Row className="justify-content-md-center">
-              <Col md="auto">
-                <img src={profilePic} alt="Profile" 
-                                      className="profile" 
-                                      onClick={clickHandler}/>
-              </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-              <Col md="auto">
-                <h1>Sarah Tao</h1>
-              </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-              <Col md="auto">
-                <a href="https://www.linkedin.com/in/sarahtao/">
-                  <FontAwesomeIcon icon={faLinkedin} size="2x"/>
-                </a>
-              </Col>
-              <Col md="auto">
-                <a href="https://github.com/s-tao">
-                  <FontAwesomeIcon icon={faGithubSquare} size="2x"/>
-                </a>  
-              </Col>
-              <Col md="auto">
-              {/* attach click event to project to display new component that shows profile desc. */}
-                <FontAwesomeIcon icon={faBuffer} size="2x"/>
+            
+            <Row>
+              <Col md={12}>
+                <Row className="justify-content-md-center">
+                  <Col md="auto" className="center-flex">
+                    <img src={profilePic} alt="Profile" 
+                                          className="profile-img" 
+                                          onClick={clickHandler}/>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="auto" className="center-flex">
+                    <h1>Sarah Tao</h1>
+                  </Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                  <Col md="auto" className="center-flex">
+                    <a href="https://www.linkedin.com/in/sarahtao/">
+                      <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+                    </a>
+                  </Col>
+                  <Col md="auto" className="center-flex">
+                    <a href="https://github.com/s-tao">
+                      <FontAwesomeIcon icon={faGithubSquare} size="2x"/>
+                    </a>  
+                  </Col>
+                  <Col md="auto" className="center-flex">
+                    <FontAwesomeIcon icon={faBuffer} size="2x"/>
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Container>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,44 +4,68 @@ import { Container, Row, Col } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithubSquare, faBuffer } from '@fortawesome/free-brands-svg-icons'
 import profilePic from '../prof-pic.jpg' 
-import Fade from 'react-reveal/Fade';
+// import '../Profile.css';
+
+// import Fade from 'react-reveal/Fade';
 
 
 const Profile = ({clickHandler}) => {
-    return (
-        <Container fluid className="homepage-info">
-            <Row className="justify-content-md-center">
-              <Col md="auto">
-                <img src={profilePic} alt="Profile" 
-                                      className="profile"
-                                      onClick={clickHandler}
 
-                />
-              </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-              <Col md="auto">
-                <h1>Sarah Tao</h1>
-                <p>Description of myself here</p>
-              </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-              <Col md="auto">
-                <a href="https://www.linkedin.com/in/sarahtao/">
-                  <FontAwesomeIcon icon={faLinkedin} size="2x"/>
-                </a>
-              </Col>
-              <Col md="auto">
-                <a href="https://github.com/s-tao">
-                  <FontAwesomeIcon icon={faGithubSquare} size="2x"/>
-                </a>  
-              </Col>
-              <Col md="auto">
-              {/* attach click event to project to display new component that shows profile desc. */}
-                <FontAwesomeIcon icon={faBuffer} size="2x"/>
-              </Col>
-            </Row>
-          </Container>
+    return (
+        <Container fluid className="homepage-info">             
+          <Row className="center-flex"> 
+            <Col md={{span: 3, offset: 1}}>
+              <Row>
+                <Col md={12} className="center-flex">
+                  <img src={profilePic} alt="Profile" 
+                                        className="profile-img"
+                                        onClick={clickHandler}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col md={12} className="center-flex">
+                  <h1>Sarah Tao</h1>
+                </Col>
+              </Row>
+              <Row className="center-flex">
+                <Col md="auto" sm={1} className="center-flex">
+                  <a href="https://www.linkedin.com/in/sarahtao/">
+                    <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+                  </a>
+                </Col>
+                <Col md="auto" sm={1} className="center-flex">
+                  <a href="https://github.com/s-tao">
+                    <FontAwesomeIcon icon={faGithubSquare} size="2x"/>
+                  </a>  
+                </Col>
+                <Col md="auto" sm={1} className="center-flex">
+                  <FontAwesomeIcon icon={faBuffer} size="2x"/>
+                </Col>
+              </Row>
+            </Col>
+            <Col md={7} className="center-flex">
+              <p>
+                I was a Landscape Designer at a firm in Marin for almost three 
+                years. During that time, I immediately gravitated towards the 
+                minute design and details of how things were constructed. I used 
+                AutoCAD on a daily basis to draft out plans and details, Sketchup
+                to model our designs, and Adobe Suite to render marketing images.
+                Although I enjoyed working with various applications, I gradually 
+                became more interested in how these programs were created to help
+                me as the user.
+              <br></br><br></br>
+                Through a series of other events, the idea that I could program 
+                slowly grew. I began entertaining different ways I can help 
+                improve people's lives. So... I made the switch through joining 
+                Hackbright Academy, an all female coding bootcamp! I am excited 
+                to continue learning and creatingthings that can improve one's 
+                quality of life in the digital space!
+              </p>
+            </Col>
+          </Row>
+        </Container>
+        
     );
 }
 
