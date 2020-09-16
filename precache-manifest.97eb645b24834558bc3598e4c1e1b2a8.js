87efbef207cd83d798dbda09f0bfc363dbae92f5
Sarah Tao <satao.94@gmail.com>
2020-09-15 20:38:10 -0700

    Updates

---
+++ precache-manifest.97eb645b24834558bc3598e4c1e1b2a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082a4410b7df1e9ca7d26d3b3124952c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86ff683e96bef4cb78f0",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "3060f1ec2960a27d8447",
+    "url": "/static/css/main.a251dda4.chunk.css"
+  },
+  {
+    "revision": "86ff683e96bef4cb78f0",
+    "url": "/static/js/2.d8df6317.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8df6317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3060f1ec2960a27d8447",
+    "url": "/static/js/main.089c26a7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2c05f82916b5fedab470b916c3744d35",
+    "url": "/static/media/clouds-bg.2c05f829.jpg"
+  },
+  {
+    "revision": "9cc396c9cf92c1bd31e49ee239f61e54",
+    "url": "/static/media/prof-pic.9cc396c9.jpg"
+  }
+]);
