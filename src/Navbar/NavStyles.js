374904ba7a591f6cb5e6a71fb66c95c73aa26498
Sarah Tao <satao.94@gmail.com>
2020-10-19 19:03:02 -0700

    added mobile styling for nav

--- src/Navbar/NavStyles.js
+++ src/Navbar/NavStyles.js
@@ -5,16 +5,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const NavContainer = styled.div`
   display: flex;
-  height: 50px;
   margin: 25px;
   align-items: center;
+  justify-content: space-between;
 `;
 
 export const Navbar = styled.div`
-  ${'' /* padding: 8px; */}
   display: flex;
-  height: 25px;
-  justify-content: start;
 `;
 
 export const LinkStyle = styled(NavLink)`
@@ -30,7 +27,6 @@ export const LinkStyle = styled(NavLink)`
 export const SocialMedia = styled.div`
   display: flex;
   height: 25px;
-  margin-left: auto;
 `;
 
 export const IconStyle = styled(FontAwesomeIcon)`
@@ -38,4 +34,40 @@ export const IconStyle = styled(FontAwesomeIcon)`
   color: ${props => props.color};
   :hover {
       color: ${props => props.hover};
+`;
+
+/* mobile responsive */
+export const MobileNavContainer = styled(NavContainer)`
+  position: fixed;
+  width: 100vw;
+  bottom: 0;
+  margin: 0;
+  padding: 12px 24px;
+  background-color: #FFFFFF;
+  -webkit-box-shadow: 0px -1px 2px rgba(230,230,230,1);
+  -moz-box-shadow: 0px -1px 2px rgba(230,230,230,1);
+  box-shadow: 0px -1px 2px rgba(230,230,230,1);
+`;
+
+export const MobileNavbar = styled(Navbar)`
+  flex: 1;
+  padding: 0;
+  justify-content: space-around;
+  font-size: 12px;
+`;
+
+export const MobileNavLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const MobileSocialMedia = styled.a`
+  color: ${props => props.color};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const MobileIconStyle = styled(IconStyle)`
 `;
--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,56 +1,126 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavContainer, 
          Navbar, 
          SocialMedia, 
          IconStyle, 
-         LinkStyle } from './NavStyles.js';
+         LinkStyle,
+         MobileNavContainer,
+         MobileNavbar,
+         MobileNavLink,
+         MobileIconStyle,
+         MobileSocialMedia
+       } from './NavStyles.js';
 import { faLinkedin, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+import { faHome, faUser, faBook } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = ({ textColor, hoverColor, activeColor }) => {
   
+  const [mobileNav, setMobileNav] = useState(null);
+  const isMobile = mobileNav <= 640;
+
+  useEffect(() => {
+    setMobileNav(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    // may switch to function dec. for global access
+    const handleResize = () => {
+      setMobileNav(window.innerWidth);
+    }
+  
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize)
+  }, []);
+
 
   return (
-    <NavContainer>
-      <Navbar>
-        <LinkStyle exact to="/" 
-                   color={textColor}
-                   hover={hoverColor}
-                   activeClassName="selected"
-                   activeStyle={{ color: activeColor }}>
-          HOME
-        </LinkStyle>  
-        <LinkStyle to="/about" 
-                   color={textColor}
-                   hover={hoverColor}
-                   activeClassName="selected"
-                   activeStyle={{ color: activeColor }}>
-          ABOUT
-        </LinkStyle> 
-        <LinkStyle to="/projects" 
-                   color={textColor}
-                   hover={hoverColor}
-                   activeClassName="selected"
-                   activeStyle={{ color: activeColor }}>
-          PROJECTS
-        </LinkStyle> 
-        </Navbar>
-  
-        <SocialMedia>
-        <a href="https://www.linkedin.com/in/sarahtao/">
-            <IconStyle icon={faLinkedin} 
-                       size="2x"
-                       color={textColor}
-                       hover={hoverColor}
-                       />
-        </a>
-        <a href="https://github.com/s-tao">
-            <IconStyle icon={faGithubSquare} 
-                       color={textColor}
-                       hover={hoverColor}
-                       size="2x"/>
-        </a>
-        </SocialMedia>
-    </NavContainer>
+    <>
+      {isMobile ? (
+        <MobileNavContainer>
+          <MobileNavbar>
+            <MobileNavLink exact to="/">
+              <MobileIconStyle icon={faHome}
+                               color={hoverColor}
+                               size="2x">
+              </MobileIconStyle>
+              Home
+            </MobileNavLink>
+            <MobileNavLink to="/about">
+              <MobileIconStyle icon={faUser}
+                               color={hoverColor}
+                               size="2x">
+              </MobileIconStyle>
+              About
+            </MobileNavLink>
+            <MobileNavLink to="/projects">
+              <MobileIconStyle icon={faBook}
+                               color={hoverColor}
+                               size="2x">
+              </MobileIconStyle>
+              Projects
+            </MobileNavLink>
+            <MobileSocialMedia a href="https://www.linkedin.com/in/sarahtao/">
+              <MobileIconStyle icon={faLinkedin}
+                               color={hoverColor}
+                               size="2x">
+              </MobileIconStyle>
+              LinkedIn
+            </MobileSocialMedia>
+            <MobileSocialMedia a href="https://github.com/s-tao">
+              <MobileIconStyle icon={faGithubSquare}
+                               color={hoverColor}
+                               size="2x">
+              </MobileIconStyle>
+              Github
+            </MobileSocialMedia>
+          </MobileNavbar>
+        </MobileNavContainer>
+      ) : (
+        <NavContainer>
+          <Navbar>
+            <LinkStyle exact to="/" 
+                      color={textColor}
+                      hover={hoverColor}
+                      activeClassName="selected"
+                      activeStyle={{ color: activeColor }}>
+              HOME
+            </LinkStyle>  
+            <LinkStyle to="/about" 
+                      color={textColor}
+                      hover={hoverColor}
+                      activeClassName="selected"
+                      activeStyle={{ color: activeColor }}>
+              ABOUT
+            </LinkStyle> 
+            <LinkStyle to="/projects" 
+                      color={textColor}
+                      hover={hoverColor}
+                      activeClassName="selected"
+                      activeStyle={{ color: activeColor }}>
+              PROJECTS
+            </LinkStyle> 
+          </Navbar>
+    
+          <SocialMedia>
+            <a href="https://www.linkedin.com/in/sarahtao/">
+                <IconStyle icon={faLinkedin} 
+                          size="2x"
+                          color={textColor}
+                          hover={hoverColor}
+                          />
+            </a>
+            <a href="https://github.com/s-tao">
+                <IconStyle icon={faGithubSquare} 
+                          color={textColor}
+                          hover={hoverColor}
+                          size="2x"/>
+            </a>
+          </SocialMedia>
+      </NavContainer>
+      )}
+      
+    </>
   );
 }
 
